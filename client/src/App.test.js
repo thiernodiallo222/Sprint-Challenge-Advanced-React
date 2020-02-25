e1352e062c69052b9faa236b1b5a573c9c9a70a6
thiernodiallo222 <thiernodiallo864@gmail.com>
2020-02-24 21:08:34 -0500

    completed project

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import useDarkMode from './Components/Hooks/useDarkMode';
+import useLocalStorage from './Components/Hooks/useLocalStorage';
+import PlayerList from './Components/PlayerList';
+import PlayerCard from './Components/PlayerCard';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+  
+it('renders without craching', () => {
+  ReactDom.render(<PlayerList />)
+});
+
+it('renders without craching', () => {
+  ReactDom.render(<PlayerCard />)
+})
